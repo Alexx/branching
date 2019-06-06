cd310819db5031dff80e6d622fb56d5407664dd5
Abdellah Ramadan and Alex Siegberg <abdellah@>
2019-06-06 10:18:37 -0700

    add basic form and check age to show different content

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 //this code will run after everything loads
 $(document).ready(function(){
-
-
+  $("#ageForm").submit(function(event){
+    var userAge = parseInt($("#age").val());
+    if(userAge >= 18 && userAge <= 21) {
+      $(".section1").show();
+    } else if(userAge > 21){
+      $(".section2").show();
+    } else {
+      $(".section3").show();
+    }
+    event.preventDefault();
+  });
 });
